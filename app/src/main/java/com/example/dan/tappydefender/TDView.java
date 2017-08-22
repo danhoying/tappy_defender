7cb8a5cae980a4f1d0c9f9fce0f5e938c0c52a6d
Dan Hoying <danhoying@gmail.com>
2017-08-22 09:21:13 -0700

    Add method to control FPS

--- app/src/main/java/com/example/dan/tappydefender/TDView.java
+++ app/src/main/java/com/example/dan/tappydefender/TDView.java
@@ -41,7 +41,11 @@ public class TDView extends SurfaceView implements Runnable {
     }
 
     private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
 
+        }
     }
 
     @Override
