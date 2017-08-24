0d378eb0e8946507426cd700ec72343bc325e982
Dan Hoying <danhoying@gmail.com>
2017-08-24 10:07:23 -0700

    Create EnemyShip class and its position/speed methods

--- app/src/main/java/com/example/dan/tappydefender/EnemyShip.java
+++ app/src/main/java/com/example/dan/tappydefender/EnemyShip.java
@@ -1,5 +1,61 @@
 package com.example.dan.tappydefender;
 
-public class EnemyShip {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
+import java.util.Random;
+
+public class EnemyShip {
+    private Bitmap bitmap;
+    private int x, y;
+    private int speed = 1;
+
+    // Detect enemies leaving the screen
+    private int maxX;
+    private int minX;
+
+    // Spawn enemies within screen bounds
+    private int maxY;
+    private int minY;
+
+    public EnemyShip(Context context, int screenX, int screenY) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        maxX = screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void update(int playerSpeed) {
+        // Move to the left
+        x -= playerSpeed;
+        x -= speed;
+
+        // Respawn when off screen
+        if (x < minX - bitmap.getWidth()) {
+            Random generator = new Random();
+            speed = generator.nextInt(10) + 10;
+            x = maxX;
+            y = generator.nextInt(maxY) - bitmap.getHeight();
+        }
+    }
 }
