dfdf937f0ee747c4e86db84ebbe058bf4f1fc1d8
Dan Hoying <danhoying@gmail.com>
2017-08-24 10:29:26 -0700

    Create enemy objects and draw to screen

--- app/src/main/java/com/example/dan/tappydefender/TDView.java
+++ app/src/main/java/com/example/dan/tappydefender/TDView.java
@@ -12,7 +12,12 @@ public class TDView extends SurfaceView implements Runnable {
 
     volatile boolean playing;
     Thread gameThread = null;
+
     private PlayerShip player;
+    public EnemyShip enemy1;
+    public EnemyShip enemy2;
+    public EnemyShip enemy3;
+
     private Paint paint;
     private Canvas canvas;
     private SurfaceHolder ourHolder;
@@ -22,20 +27,34 @@ public class TDView extends SurfaceView implements Runnable {
         ourHolder = getHolder();
         paint = new Paint();
         player = new PlayerShip(context, x, y);
+        enemy1 = new EnemyShip(context, x, y);
+        enemy2 = new EnemyShip(context, x, y);
+        enemy3 = new EnemyShip(context, x, y);
     }
 
     private void update() {
         player.update();
+        enemy1.update(player.getSpeed());
+        enemy2.update(player.getSpeed());
+        enemy3.update(player.getSpeed());
     }
 
     private void draw() {
         if (ourHolder.getSurface().isValid()) {
             // Lock the area of memory being drawn to
             canvas = ourHolder.lockCanvas();
+
             // Clear the last frame
             canvas.drawColor(Color.argb(255, 0, 0, 0));
+
             // Draw the player
             canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), paint);
+
+            // Draw the enemies
+            canvas.drawBitmap(enemy1.getBitmap(), enemy1.getX(), enemy1.getY(), paint);
+            canvas.drawBitmap(enemy2.getBitmap(), enemy2.getX(), enemy2.getY(), paint);
+            canvas.drawBitmap(enemy3.getBitmap(), enemy3.getX(), enemy3.getY(), paint);
+
             // Unlock and draw the scene
             ourHolder.unlockCanvasAndPost(canvas);
         }
